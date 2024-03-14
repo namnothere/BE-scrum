6ee3c838b976071f950902b343a1b1e8e47692d4
TrungNguyen083 <91688944+TrungNguyen083@users.noreply.github.com>
2024-03-14 13:59:51 +0700

    User info (#2)
    * get user info
    
    * hotfix
    
    * user management
    
    ---------
    
    Co-authored-by: nem <canhnamtran216@gmail.com>

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth';
+import { ReqContext } from '../../shared/request-context/req-context.decorator';
+import { RequestContext } from '../../shared/request-context/request-context.dto';
+import { BaseApiResponse } from '../../shared/dtos';
+import { UserService } from '../providers';
+import { UserInfoUpdateInput } from '../dtos/user-infor-update-input.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
-  @Get()
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private readonly userService: UserService) {}
+
+  @Get('getInfo')
+  getUserInfo(@ReqContext() ctx: RequestContext): Promise<BaseApiResponse<any>> {
+    return this.userService.getUserInfo(ctx.user.id);
+  }
+
+  @Put('updateInfo')
+  updateUserInfo(@ReqContext() ctx: RequestContext, @Body() input: UserInfoUpdateInput): Promise<BaseApiResponse<any>> {
+    return this.userService.updateUserInfo(ctx.user.id, input);
   }
 }
