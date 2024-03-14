0abac33ce154d50304568b80be94c4a4a5e7d745
nem <canhnamtran216@gmail.com>
2024-03-14 11:16:48 +0700

    hotfix

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,11 +1,23 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../../auth';
+import { AuthService, JwtAuthGuard } from '../../auth';
+import { ReqContext } from '../../shared/request-context/req-context.decorator';
+import { RequestContext } from '../../shared/request-context/request-context.dto';
+import { BaseApiResponse } from '../../shared/dtos';
 
 @UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
+
+  constructor(private readonly authService: AuthService) {}
+
   @Get()
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Get('getInfo')
+  getUserInfo(@ReqContext() ctx: RequestContext): Promise<BaseApiResponse<any>> {
+    console.log(ctx);
+    return this.authService.getUserInfo(ctx.user.id);
   }
 }
